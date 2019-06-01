bb205ddc6962175824122409a781707c3653b45b
ISKinGeR <51255680+ISKinGeR@users.noreply.github.com>
2019-06-02 01:39:10 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -119,7 +119,7 @@ client2.on('message', message => {
 
 
 if (command == "تحدث") {
-let rank = message.guild.member(message.author).roles.find('name', 'Role.Kahrbaa');
+let rank = message.guild.member(message.author).roles.find('name', '3');
 if (!rank) return message.reply(' ')
   message.channel.send(args.join("  "))
     message.delete();
